cf13ee3ec668b90318ddf185708b7cac66a1362f
nanter1986 <nanter1986@gmail.com>
2017-08-30 11:07:43 +0300

    -fixed player spritesheet

--- core/src/com/nanter1986/blockpusher/Character/PlayerOne.java
+++ core/src/com/nanter1986/blockpusher/Character/PlayerOne.java
@@ -26,19 +26,19 @@ public class PlayerOne extends MovableCharacter {
         Gdx.app.log("drwdir",dir.toString());
         switch (dir){
             case UP:
-                b.draw(new TextureRegion(playerOne),characterX-characterW,characterY,playerOne.getWidth()/2.0f,playerOne.getHeight()/2.0f,characterW,characterH,1f,1f,90f,true);
+                b.draw(playerOne,characterX,characterY,characterW,characterH,0,0,64,64,false,false);
                 //b.draw(playerOne,characterX,characterY,characterW,characterH);
                 break;
             case DOWN:
-                b.draw(new TextureRegion(playerOne),characterX,characterY,playerOne.getWidth()/2.0f,playerOne.getHeight()/2.0f,characterW,characterH,1f,1f,-90f,true);
+                b.draw(playerOne,characterX,characterY,characterW,characterH,0,192,64,64,false,false);
                 break;
             case LEFT:
-                b.draw(new TextureRegion(playerOne),characterX,characterY,playerOne.getWidth()/2.0f,playerOne.getHeight()/2.0f,characterW,characterH,1f,1f,0f,false);
+                b.draw(playerOne,characterX,characterY,characterW,characterH,0,128,64,64,false,false);
 
                 break;
 
             case RIGHT:
-                b.draw(new TextureRegion(playerOne),characterX-characterW,characterY,playerOne.getWidth()/2.0f,playerOne.getHeight()/2.0f,characterW,characterH,1f,1f,0f,true);
+                b.draw(playerOne,characterX,characterY,characterW,characterH,0,64,64,64,false,false);
 
                 break;
 
