aab051410102f9553d9c7db747182372f3144f22
nanter1986 <nanter1986@gmail.com>
2017-10-13 10:05:44 +0300

    fixing crush only in middle of block

--- core/src/com/nanter1986/blockpusher/Character/Bosses/BossUtilities/BossCrushChecker.java
+++ core/src/com/nanter1986/blockpusher/Character/Bosses/BossUtilities/BossCrushChecker.java
@@ -18,7 +18,54 @@ public class BossCrushChecker {
     }
 
     public void checkIfcrushed(MapOne map) {
-        if (map.mapArray[character.getFixatedX()][character.getFixatedY()].type != BlockGeneral.Blocktypes.AIR) {
+        int xForArray = 0;
+        int yForArray = 0;
+        final float LIMIT = 0.2f;
+        float xTocheck = (character.coord.realX % character.characterW) / character.characterW;
+        float yTocheck = (character.coord.realY % character.characterW) / character.characterW;
+        Gdx.app.log("crush check", xTocheck + "/" + yTocheck);
+        switch (character.dir) {
+            case UP:
+                if (yTocheck + character.characterW / 2 < LIMIT) {
+                    yForArray = character.coord.fixatedY;
+                    xForArray = character.coord.fixatedX;
+                } else {
+                    yForArray = character.coord.fixatedY + 1;
+                    xForArray = character.coord.fixatedX;
+                }
+                break;
+            case DOWN:
+                if (yTocheck - character.characterW < LIMIT) {
+                    yForArray = character.coord.fixatedY - 1;
+                    xForArray = character.coord.fixatedX;
+                } else {
+                    yForArray = character.coord.fixatedY;
+                    xForArray = character.coord.fixatedX;
+                }
+                break;
+            case LEFT:
+                if (xTocheck - character.characterW < LIMIT) {
+                    yForArray = character.coord.fixatedY;
+                    xForArray = character.coord.fixatedX - 1;
+                } else {
+                    yForArray = character.coord.fixatedY;
+                    xForArray = character.coord.fixatedX;
+                }
+                break;
+            case RIGHT:
+                if (xTocheck + character.characterW < LIMIT) {
+                    yForArray = character.coord.fixatedY;
+                    xForArray = character.coord.fixatedX;
+                } else {
+                    yForArray = character.coord.fixatedY;
+                    xForArray = character.coord.fixatedX + 1;
+                }
+
+                break;
+
+        }
+        Gdx.app.log("crush check int", xForArray + "/" + yForArray);
+        if (map.mapArray[xForArray][yForArray].type != BlockGeneral.Blocktypes.AIR) {
             character.crushed = true;
             character.explodedStarted=true;
             Gdx.app.log("enemy crushed:", character.crushed + " is dead");
